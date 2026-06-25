696071ac3fd26deff442cfcf724a33ded00f1896
KristiyanYanakiev <yankris_92@yahoo.com>
2026-06-25 17:42:48 +0300

    more solutions

--- exercise_django_auths/accounts/urls.py
+++ exercise_django_auths/accounts/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accounts import views
+from common.views import UserDetailsPage
 
 app_name = 'accounts'
 urlpatterns = [
@@ -9,6 +10,7 @@ urlpatterns = [
     path('logout/', views.logout_fbv, name='logout'),
     path('register/', views.register_fbv, name='register'),
     path('login_cbv/', LoginView.as_view(template_name='accounts/fbv_views/login_fbv.html'), name='login_cbv'),
-    path('logout_cbv/', LogoutView.as_view(), name='logout_cbv')
+    path('logout_cbv/', LogoutView.as_view(), name='logout_cbv'),
+    path('details/', UserDetailsPage.as_view(), name='details')
 
 ]
--- exercise_django_auths/common/views.py
+++ exercise_django_auths/common/views.py
@@ -1,13 +1,16 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
-    ...
+
+    print(get_user_model())
+
 
 # @login_required
 # def home_fb(request: HttpRequest) -> HttpResponse:
 #     return render(request, 'home.html')
+
+class UserDetailsPage(TemplateView):
+    template_name = 'accounts/user_details.html'
